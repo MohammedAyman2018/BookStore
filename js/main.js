0a84790041199313caef288c5d204c6f37ee97f8
Mohammed Ayman <kookoo_1970@yahoo.com>
2019-04-20 18:16:38 +0200

    Waiting for declaration

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    //nav li 
+    // $('.navbar-header button').click(function (e) { 
+    //     e.preventDefault();
+    //     $('.navbar-default .navbar-collapse').css({
+    //         'position': 'fixed',
+    //         'top': '100px',
+    //         'left': '0',
+    //         'width': '100%',
+    //         'height': '100%'
+    //     })
+    // });
+    // nice-selct
+    $('select').niceSelect();
+});
