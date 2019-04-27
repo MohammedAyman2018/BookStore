afd7e28fec9cb2b6e20dfbdabab4a1fe69eb2b51
Mohammed Ayman <kookoo_1970@yahoo.com>
2019-04-28 00:15:35 +0200

    Fix Problems

--- js/main.js
+++ js/main.js
@@ -1,15 +1,9 @@
 $(document).ready(function() {
-    //nav li 
-    // $('.navbar-header button').click(function (e) { 
-    //     e.preventDefault();
-    //     $('.navbar-default .navbar-collapse').css({
-    //         'position': 'fixed',
-    //         'top': '100px',
-    //         'left': '0',
-    //         'width': '100%',
-    //         'height': '100%'
-    //     })
-    // });
     // nice-selct
     $('select').niceSelect();
+    
+    // Bootstrap modal
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').focus()
+    });
 });
