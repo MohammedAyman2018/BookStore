702d01e77e98d464dcd60e49e437db30b1a7a566
Mohammed Ayman <kookoo_1970@yahoo.com>
2019-04-28 21:42:15 +0200

    More orgenize && Add Book's section

--- js/main.js
+++ js/main.js
@@ -6,4 +6,11 @@ $(document).ready(function() {
     $('#myModal').on('shown.bs.modal', function () {
         $('#myInput').focus()
     });
+
+    // Popover
+    $(function () {
+        $('[data-toggle="popover"]').popover({
+            html: true
+        })
+    });
 });
